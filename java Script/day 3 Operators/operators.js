e76797609d15735bc7d949cb948a920c97e02ef0
Anuj Kumar Verma <vanuj545@gmail.com>
2024-07-27 11:41:04 +0530

    second commit

---
+++ java Script/day 3 Operators/operators.js
@@ -0,0 +1,15 @@
+/* ----- Increment and decrement Operator */
+     
+   // Pre Increment
+
+let a = 20;
+      a= ++a;
+      a= ++a;
+      a= ++a;
+
+ let b = ++a;
+       
+       ++b;
+
+ console.log(a);
+ console.log(b);
